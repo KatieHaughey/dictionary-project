73026b33e05d6d73651e039d1cfc219be94d9b66
KatieHaughey <132608324+KatieHaughey@users.noreply.github.com>
2023-07-10 16:02:58 +0100

    added dictionary api integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header">Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a
+              href="https://katiehaughey.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Katie Haughey
+            </a>{" "}
+            and open-sourced on{" "}
+            <a
+              href="https://github.com/KatieHaughey/dictionary-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
